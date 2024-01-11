ebabca0d363d7315fd939d198253e73920f63b35
OleksandrShulha <106068375+OleksandrShulha@users.noreply.github.com>
2024-01-11 15:20:46 +0200

    Animation Move and Idle

--- RPG/Assets/Player.cs
+++ RPG/Assets/Player.cs
@@ -6,27 +6,68 @@ public class Player : MonoBehaviour
 {
     private float xInput;
     private Rigidbody2D rb;
+    private Animator anim;
+    private int facingDir = 1;
+    private bool facingRight = true;
+
+
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpPower = 5f;
+
 
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
+        anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        MoveControler();
+        AnimationControler();
+        FlipControler();
+    }
+
+    private void MoveControler()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
-
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+    }
+
+    private void AnimationControler()
+    {
+        bool isMoving = xInput != 0;
+        anim.SetBool("isMove", isMoving);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipControler()
+    {
+        if(rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
         }
     }
 }
