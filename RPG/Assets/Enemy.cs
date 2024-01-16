e6ee333afc356db9f4c3676aed1a3347e0f9b773
OleksandrShulha <106068375+OleksandrShulha@users.noreply.github.com>
2024-01-16 11:59:29 +0200

    Enemy Atack

--- RPG/Assets/Enemy.cs
+++ RPG/Assets/Enemy.cs
@@ -6,6 +6,14 @@ public class Enemy : Entety
 {
 
     [SerializeField] private float moveSpeed = 5f;
+
+
+    public RaycastHit2D isPlayrDetected;
+    [SerializeField] private float playrChekDistanse;
+    [SerializeField] private LayerMask whatIsPlayr;
+
+    private bool isAtack;
+
     protected override void Start()
     {
         base.Start();
@@ -15,15 +23,56 @@ public class Enemy : Entety
     {
         base.Update();
         MoveControler();
-        Debug.Log("Земля у врага: " + isGrounded);
+        anim.SetBool("isAtack", isAtack);
+
+
     }
 
     private void MoveControler()
     {
-        rb.velocity = new Vector2(facingDir * moveSpeed, rb.velocity.y);
+
+        if (isPlayrDetected)
+        {
+            Debug.Log("i see you");
+            if (isPlayrDetected.distance > 1)
+            {
+                rb.velocity = new Vector2(facingDir * 5f * moveSpeed, rb.velocity.y);
+                isAtack = false;
+            }
+            else
+            {
+                isAtack = true;
+            }
+        }
+        else
+        {
+            Debug.Log("i not see");
+            isAtack = false;
+        }
+
+
+        if(!isAtack && !isPlayrDetected)
+            rb.velocity = new Vector2(facingDir * moveSpeed, rb.velocity.y);
+
+
+
         if (!isGrounded || isWallDetected)
         {
             Flip();
         }
+    }
+
+    protected override void CollisionCheks()
+    {
+        base.CollisionCheks();
+
+        isPlayrDetected = Physics2D.Raycast(transform.position, Vector2.right, playrChekDistanse * facingDir, whatIsPlayr);
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + playrChekDistanse * facingDir, transform.position.y));
     }
 }
